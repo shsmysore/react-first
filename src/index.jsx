ec2e5484eb104f42aecab4b27481e51bc541e733
Shree Harsha <shsmysore@gmail.com>
2025-04-06 18:57:17 +0530

    Switch to VS Code and format the lines.

--- src/index.jsx
+++ src/index.jsx
@@ -4,26 +4,26 @@ const rootDom = createRoot(document.getElementById('root'))
 
 function Header() {
     return (
-            <img src='/src/assets/react-png.png' width='40px'/>
-        )
+        <img src='/src/assets/react-png.png' width='40px' />
+    )
 }
 
 function MainContent() {
-    return(
-            <>
-               <h1> Real facts about React! </h1>
-               <ul>
-                   <li> Was originally created in 2013 </li>
-                   <li> It is maintained by Meta </li>
-                   <li> React is Composable </li>
-                   <li> React is Declarative, not imperative. </li>
-               </ul>
-            </>
-        )
+    return (
+        <>
+            <h1> Real facts about React! </h1>
+            <ul>
+                <li> Was originally created in 2013 </li>
+                <li> It is maintained by Meta </li>
+                <li> React is Composable </li>
+                <li> React is Declarative, not imperative. </li>
+            </ul>
+        </>
+    )
 }
 
-function Footer()  {
-    return(
+function Footer() {
+    return (
         <small>© 2024 Shree development. All rights reserved. </small>
     )
 }
